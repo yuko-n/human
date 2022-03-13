5fc664f8823fe30874654fa0382c9b344466fd38
yuko-n <98873563+yuko-n@users.noreply.github.com>
2022-03-14 03:30:48 +0700

    done

--- human.cpp
+++ human.cpp
@@ -2,55 +2,85 @@
 #include <string.h>
 #include <locale.h>
 #include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <ctime>
 
 using namespace std;
 
-class WALK {
+class GCS {
+	protected:
+		double step;
+		double speed;
 	public:
-	int speed;
-	WALK() {
-		speed = rand()%5 + 1;
-	}
-	//set_speed()
-	int get_speed() {
-		return speed;
-	}
-};
+		double longtitude;
+		double latitude;
+		vector<double> miliage;
+		vector<double> longs;
+		vector<double> lats;
+		double path;
+		double way;
+		GCS (double longt, double latit) {
+			if (longt >= 0 && longt <= 180) {
+				longtitude = longt;
+				longs.push_back(longtitude);
+			}
+			else {
+				cout << "ОШИБКА: Неверно задана долгота" << endl;
+				longtitude = -1;
+			}
+			if (latit >= 0 && latit <= 90) {
+				latitude = latit;
+				lats.push_back(latitude);
+			}	
+			else {
+				cout << "ОШИБКА: Неверно задана широта" << endl;
+				latitude = -1;
+			}
+			way = 0;
+			path = 0;
+		}
+		
+		void set_gcs (double longt, double latit) {
+			longtitude = longt;
+			latitude = latit;
+		}
+		
+		double get_longtitude() {
+			return longtitude;
+		}
+		
+		double get_latitude() {
+			return latitude;
+		}
+		
+		void get_gcs() {
+			cout << endl << "Долгота: " << get_longtitude() << endl << "Широта: " << get_latitude() << endl;
+		}
 
-class GCS : public WALK {
-	public:
-	double longtitude;
-	double latitude;
-	GCS(double longt, double latit) : WALK() {
-		if (longt >= 0 && longt <= 180) longtitude = longt;
-		else {
-			cout << "ОШИБКА: Неверно задана долгота" << endl;
-			longtitude = -1;
+		void rand_walk() {
+			speed = (rand()%5 + 1);
+			int quarter_1 = rand() % 4 + 1;
+			int quarter_2 = rand() % 4 + 1;
+			double new_long = longtitude;
+			double new_lat = latitude;
+			latitude = latitude + pow(-1, quarter_1)*(0.009*speed*step);
+			longtitude = longtitude + pow(-1, quarter_2)*(0.009*speed*step);
+			printf("%lf\t%lf\t%lf\t%lf\n", longtitude, latitude, step, speed);
+			longs.push_back(longtitude); 
+			lats.push_back(latitude);
+			walk_mileage(new_long, new_lat);
 		}
-		if (latit >= 0 && latit <= 90) latitude = latit;
-		else {
-			cout << "ОШИБКА: Неверно задана широта" << endl;
-			latitude = -1;
+
+		void walk_mileage(double lon1, double lat1) {
+			path += 111.2*sqrt((abs(lon1 - longtitude))*(abs(lon1 - longtitude)) + (abs(lat1 - latitude))*cos(3.14*lon1/180)*(abs(lat1 - latitude))*cos(3.14*lon1/180));
+			way += path;
+			miliage.push_back(path);
 		}
-	}
-	
-	void set_gcs(double longt, double latit) {
-		longtitude = longt;
-		latitude = latit;
-	}
-	
-	double get_longtitude() {
-		return longtitude;
-	}
-	
-	double get_latitude() {
-		return latitude;
-	}
-	
-	void get_gcs() {
-		cout << "\nДолгота: " << get_longtitude() << endl;
-		cout << "Широта: " << get_latitude() << endl;
-	}
+		
+		double get_way() {
+			return way;
+		}
 };
 
 class HUMAN : public GCS {
@@ -60,18 +90,28 @@ class HUMAN : public GCS {
 		string middle_name;
 		double weight;
 		double height;
-		int speed;
-
+        int life_time;
 	public:	
-		HUMAN(string fn, string sn, string mn, double w, double h, double longt, double latit) : GCS(longt, latit){
+		HUMAN(string fn, string sn, string mn, double w, double h, double longt, double latit, int life) : GCS (longt, latit) {
 			first_name = fn;
 			second_name = sn;
 			middle_name = mn;
 			weight = w;
 			height = h;
-			speed = get_speed();
+            life_time = life;
+			step = h/4 + 0.00037;
+		}
+
+		void set_name(string fn, string sn, string mn) {
+			first_name = fn;
+			second_name = sn;
+			middle_name = mn;
 		}
 		
+		void set_parameters(double w, double h) {
+			weight = w;
+			height = h;
+		}
 		string get_fn() {
 			return first_name;
 		}
@@ -85,19 +125,15 @@ class HUMAN : public GCS {
 		}
 		
 		void get_name() {
-			cout << "ФИО: " << get_sn();
-			cout << " " << get_fn();
-			cout << " " << get_mn();
-		}
-		
-		void set_name(string fn, string sn, string mn){
-			first_name = fn;
-			second_name = sn;
-			middle_name = mn;
+			cout << "ФИО: " << get_sn() << " " << get_fn() << " " << get_mn();
 		}
 		
 		double get_weight() {
 			return weight;
+		}
+		
+		double get_step() {
+			return step;
 		}
 		
 		double get_height() {
@@ -105,23 +141,31 @@ class HUMAN : public GCS {
 		}
 		
 		void get_parameters() {
-			cout << "\nВес: " << get_weight() << endl;
+			cout << endl << "Вес: " << get_weight() << endl;
 			cout << "Рост: " << get_height() << endl;
 		}
 		
-		void set_parameters(double w, double h) {
-			weight = w;
-			height = h;
+		double get_speed() {
+            return speed;
+        }
+		
+        int get_life() {
+            return life_time;
+        }
+		
+		void walking() {
+			for (int i=0; i<life_time;i++) {
+				rand_walk();
+			}
 		}
 };
 
 int main() {
+    srand(time(NULL));
+    setlocale(LC_ALL, "RUS");
 	double w, h;
-	HUMAN *human_1 = new HUMAN("Ярослав", "Обухович", "Евгеньевич", 79, 186, 55, 82.9);
-	human_1->get_name();	
-	human_1->get_parameters();
-	human_1->get_gcs();
-	cout << "Скорость движения: " << human_1->get_speed() << " км/ч"<< endl;
+	HUMAN *human_1 = new HUMAN("Петр", "Петров", "Петрович", 79, 0.00186, 55, 82.9, 5);
+	human_1->walking();
+	printf("%lf\n", human_1->get_way());
 	cout << endl;
-
 }
